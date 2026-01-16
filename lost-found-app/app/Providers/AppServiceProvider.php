f01fe19fd8ff545b1619730a9419bc75a2369684
Guptantun <guptan2777@gmail.com>
2026-01-16 14:30:35 +0700

    Force HTTPS schema

--- lost-found-app/app/Providers/AppServiceProvider.php
+++ lost-found-app/app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL; // 👈 1. เพิ่มบรรทัดนี้ที่ด้านบนสุด (ใต้ namespace)
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // 👇 2. เพิ่มก้อนนี้ลงไปในฟังก์ชัน boot
+        if (config('app.env') === 'production') {
+            URL::forceScheme('https');
+        }
     }
 }
